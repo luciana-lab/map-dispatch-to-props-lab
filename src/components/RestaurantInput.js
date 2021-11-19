18576a9978837eba03eca18d87735e34185892d1
Luciana Sugeta <lulusugeta@gmail.com>
2021-11-19 18:18:12 -0500

    commit tree e6ce3c36215d75d81402d077064891d50b714806

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -24,10 +24,11 @@ export class RestaurantInput extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     // add missing code
+    this.props.addRestaurant(this.state)
   }
 
   render() {
-    return(
+    return (
       <form onSubmit={(event) => this.handleOnSubmit(event)}>
         <p>
           <input
@@ -49,6 +50,14 @@ export class RestaurantInput extends Component {
   }
 };
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addRestaurant: restaurant => {
+      dispatch(addRestaurant(restaurant))
+    }
+  }
+}
+
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(null, mapDispatchToProps)(RestaurantInput)
